6c0a8b9049d27143a272fed28e62767b5b44635e
Patrick Hurley <phurley@gmail.com>
2006-08-08 16:46:09 +0000

    Fix example#2
    git-svn-id: http://ruby-snarl.rubyforge.org/svn/trunk@9 4cdc1f07-391a-0410-901e-8ba36eb30a96

--- examples/example2.rb
+++ examples/example2.rb
@@ -3,12 +3,12 @@ require 'snarl'
 puts "Snarl Version: #{Snarl.version}"
 
 10.downto(1) do |i|
-  Snarl.show_message("Message", "Counting down #{i}", i)
+  Snarl.show_message("Message", "Counting down #{i}", nil, i)
 end
 
 sleep 11
 
 10.times do |i|
-  Snarl.show_message("Message", "Counting down #{i+1}", i+1)
+  Snarl.show_message("Message", "Counting down #{i+1}", nil, i+1)
 end
 
--- ruby-snarl.gemspec
+++ ruby-snarl.gemspec
@@ -2,7 +2,7 @@ require 'rubygems'
 
 spec = Gem::Specification.new do |s|
   s.name = "ruby-snarl"
-  s.version = "0.0.7"
+  s.version = "0.0.8"
   s.author = "Patrick Hurley"
   s.email = "phurley@gmail.com"
   s.homepage = "http://ruby-snarl.rubyforge.org/"
